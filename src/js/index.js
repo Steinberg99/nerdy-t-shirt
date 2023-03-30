6ecaff48056048b998a6ef62990cc9e8b923f8d9
Steinberg99 <stein@schaapontwerpers.nl>
2023-03-30 14:50:46 +0200

    Added change of color and tex

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 const localStorageInputs = document.querySelectorAll(".local-storage-input");
 const options = ["fit-options", "size-options", "color-options"];
+const textInput = document.querySelector(".text-input");
+const tShirtText = document.querySelector(".t-shirt-text");
+const colorInputs = document.querySelectorAll(".color-input");
+const tShirt = document.querySelector(".t-shirt");
 
 const writeLocalStorage = () => {
   if (Storage === void 0) return; // Return if local storage is not supported by the browser
@@ -8,12 +12,22 @@ const writeLocalStorage = () => {
     // Add event listeners to all of the local storage inputs
     input.addEventListener("change", (e) => {
       localStorage.setItem(input.name, input.value);
+
+      console.log(input.name);
+      console.log(localStorage.getItem(input.name));
     });
   });
 };
 
 const readLocalStorage = () => {
   if (Storage === void 0) return; // Return if local storage is not supported by the browser
+
+  const text = localStorage.getItem("text-option");
+
+  if (text) {
+    tShirtText.textContent = text;
+    textInput.value = text;
+  }
 
   options.forEach((option) => {
     const value = localStorage.getItem(option);
@@ -24,5 +38,21 @@ const readLocalStorage = () => {
   });
 };
 
+const setTShirtColor = () => {
+  const color = localStorage.getItem("color-options");
+
+  if (!color) return;
+
+  tShirt.style.fill = color;
+};
+
 writeLocalStorage();
 readLocalStorage();
+setTShirtColor();
+
+// Add event listeners
+textInput.addEventListener("input", (e) => (tShirtText.textContent = e.target.value));
+
+colorInputs.forEach((input) => {
+  input.addEventListener("change", (e) => (tShirt.style.fill = e.target.value));
+});
